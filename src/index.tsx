c9ed70ce31223e08ca00c0dc474110b271a8e7ad
Evgeniy <blednovea@smartguide.agency>
2019-07-11 17:43:02 +0300

    Add tslint rules

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import { Hello } from "./components/Hello";
+import { Hello } from './components/Hello'
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("app")
-);
+    <Hello compiler='TypeScript' framework='React' />,
+    document.getElementById('app')
+)
