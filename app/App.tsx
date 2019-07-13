bf08f36579bd627be8dafd1dcdbd3225d33b6a89
Evgeniy <blednovea@smartguide.agency>
2019-07-13 19:19:24 +0300

    Initialize firebase

--- app/App.tsx
+++ app/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import 'typeface-roboto';
-import { configureStore } from '~/frameworks';
+import { configureStore, initializeFirebase } from '~/frameworks';
 import Root from '~/Root';
 import injectSheet from '~/styles/reset';
+
+initializeFirebase();
 
 const App = () => (
   <Provider store={configureStore()}>
